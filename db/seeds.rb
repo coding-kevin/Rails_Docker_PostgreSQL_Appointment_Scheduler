cd4c1f5241dcdaa2d3d2a3b8cdef6e8baec21b63
coding-kevin <kevinmcl1277@icloud.com>
2022-08-16 21:22:58 -0700

    get seed script in basic working order. Need to prevent conflicting appointments. Can add non-desctructive seed method as well

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,38 @@
 require 'faker'
 
+Appointment.destroy_all
+Patient.destroy_all
+Doctor.destroy_all
+
 10.times do
-#doctors
+doctor = Doctor.create(
+    name: Faker::Name.unique.name
+)
     10.times do
-        #patients per doc
+        patient = Patient.create(
+            doctor_id: doctor.id,
+            name: Faker::Name.unique.name
+        )
         5.times do
-            #past appointments
+            # start_time_raw = Faker::Time.unique.forward(days: 1826)
+            # start_time_iso = 
+            past_appointments = Appointment.create(
+                patient_id: patient.id,
+                doctor_id: patient.doctor_id,
+                duration_in_minutes: 50,
+                start_time: Faker::Time.unique.backward(days: 1826)
+            )
         end
 
         5.times do 
-            #future appointments
+            future_appointments = Appointment.create(
+                patient_id: patient.id,
+                doctor_id: patient.doctor_id,
+                duration_in_minutes: 50,
+                start_time: Faker::Time.unique.forward(days: 1826)
+            )
         end
     end
 end
 
-p "Created #{doctors.count} doctors, #{patients.count} patients, #{appointments.count} appointments."
+# p "Created #{doctor.count} doctors, #{patient.count} patients, #{past_appointments.count} past appointments, #{future_appointments} future appointments."
