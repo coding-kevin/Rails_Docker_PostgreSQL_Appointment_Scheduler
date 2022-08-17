93dd888182442cb0f253b67fb4f10fdd6f2dda17
coding-kevin <kevinmcl1277@icloud.com>
2022-08-16 19:20:28 -0700

    outline seeding file and doctors controller

---
+++ app/controllers/api/doctors_controller.rb
@@ -0,0 +1,13 @@
+class Api::AppointmentsController < ApplicationController
+    def index
+    # Requirement 4: Create a new endpoint that returns all doctors that do not have an appointment.
+      # TODO: return all values
+      # TODO: return filtered values
+      head :ok
+    end
+
+    def create
+    #Requirement 5: create new appointment POST to api/appointments
+      # TODO:
+    end
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,17 @@
-# TODO: Seed the database according to the following requirements:
-# - There should be 10 Doctors with unique names
-# - Each doctor should have 10 patients with unique names
-# - Each patient should have 10 appointments (5 in the past, 5 in the future)
-#   - Each appointment should be 50 minutes in duration
+require 'faker'
+
+10.times do
+#doctors
+    10.times do
+        #patients per doc
+        5.times do
+            #past appointments
+        end
+
+        5.times do 
+            #future appointments
+        end
+    end
+end
+
+p "Created #{doctors.count} doctors, #{patients.count} patients, #{appointments.count} appointments."
